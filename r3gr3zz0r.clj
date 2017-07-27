26eb18b6fb5b2ddd8c0b8cb24aa87913a81a03ae
mattias.thalen <mattias.thalen@gmail.com>
2017-07-27 14:10:02 -0400

    message

--- r3gr3zz0r.clj
+++ r3gr3zz0r.clj
@@ -11,7 +11,7 @@
 (def rand_y (unique-random-numbers 100))
 
 ;define r3gr3zz0r
-(defn r3gr3zz0r [known xs ys & [predict_x]]
+(defn r3gr3zz0r [known xs ys x_or_y]
   ;define sums
   (let [x_sum (reduce + xs)
         y_sum (reduce + ys)
@@ -22,7 +22,13 @@
         intercept (float (/ (- (* n xy_sum) (* x_sum y_sum)) (- (* n xx_sum) (* x_sum x_sum))))]
     
     ;check if predicting x or y
-     (if predict_x (/ (- known slope) intercept) (+ slope (* intercept known)))))
+     (if (= "x" (clojure.string/lower-case x_or_y))
+       (println (str "y should be: " (+ slope (* intercept known))))
+       (println (str "x should be: " (/ (- known slope) intercept))))))
 
 ;testing
-(r3gr3zz0r 123 rand_x rand_y true)
+(r3gr3zz0r
+  (do (println "What's the known number?") (flush) (Integer. (re-find  #"\d+" (read-line))))
+  rand_x
+  rand_y
+  (do (println "Is that x or y?") (flush) (read-line)))
