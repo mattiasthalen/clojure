bc4e597b5e9821fb752bf1c58453f6e8a20a1fb4
mattias.thalen <mattias.thalen@gmail.com>
2017-07-27 15:02:56 -0400

    message

--- linear_regression.clj
+++ linear_regression.clj
@@ -1,4 +1,5 @@
 (require '[clojure.set :as set])
+(require '[clojure.string :as string])
 
 ;random number generator
 (defn unique-random-numbers [n]
@@ -17,18 +18,29 @@
         y_sum (reduce + ys)
         xy_sum (reduce + (map * xs ys))
         xx_sum (reduce + (map (fn [n] (* n n)) xs))
+        yy_sum (reduce + (map (fn [n] (* n n)) ys))
         n (count xs)
+        
         slope (float (/ (- (* y_sum xx_sum) (* x_sum xy_sum)) (- (* n xx_sum) (* x_sum x_sum))))
         intercept (float (/ (- (* n xy_sum) (* x_sum y_sum)) (- (* n xx_sum) (* x_sum x_sum))))
+        r (/ (- (* n xy_sum) (* x_sum y_sum)) (Math/sqrt (* (- (* n xx_sum) (* x_sum x_sum)) (- (* n yy_sum) (* y_sum y_sum)))))
+        r2 (* r r)
+        
         y_ (+ slope (* intercept known))
         x_ (/ (- known slope) intercept)]
     
     ;check if predicting x or y
-     (if (= "x" (clojure.string/lower-case x_or_y))
+     (if (= "x" (string/lower-case x_or_y))
        ;print y' formula
-       (println (str y_ " = " slope " + " intercept " * " known))
+       (println (str "y' = " y_))
        ;print x' formula
-       (println (str x_ " = " intercept " / (" known " - " slope ")")))))
+       (println (str "x' = " x_)))
+
+    ;print stats
+    (println (str "slope = " slope))
+    (println (str "intercept = " intercept))
+    (println (str "r2 = " r2))))
+
 
 ;trigger regression
 (linear_regression
