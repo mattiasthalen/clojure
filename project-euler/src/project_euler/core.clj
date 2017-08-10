8b26f8b483995d7d949bb5147553497b52a5adb5
mattias.thalen <mattias.thalen@gmail.com>
2017-08-10 14:46:41 -0400

    added problem 8 & 9

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -3,6 +3,11 @@
             [clojure.math.combinatorics :as combo]
             [clojure.string :as string]))
 
+(defn sum
+  "Return sum of coll"
+  [xs]
+  (reduce + xs))
+
 (defn problem-1
   "Find the sum of all the multiples of 3 or 5 below 1000."
   ([]
@@ -12,7 +17,7 @@
         (filter #(zero? (min (mod % 3)
                              (mod % 5)))
                 ,,,)
-        (reduce + ,,,))))
+        (sum ,,,))))
 
 (def fibs
   "Generate list of fibonacci numbers"
@@ -28,7 +33,7 @@
    (->> fibs
         (filter even? ,,,)
         (take-while (partial >= x) ,,,)
-        (reduce + ,,,))))
+        (sum ,,,))))
 
 (defn prime?
   "Validate if a number is a prime number"
@@ -60,7 +65,7 @@
   ([x]
    (->> x
         (prime-factors ,,,)
-        (apply max ,,,))))
+        (reduce max ,,,))))
 
 (defn palindrome? 
   "Check if input is a palindrome"
@@ -116,19 +121,24 @@
   ([x]
    (reduce math/lcm (range 1 x))))
 
+(defn square
+  "Return the square of x"
+  [x]
+  (* x x))
+
 (defn sum-of-squares 
   "Return the sum of all squares"
   [xs]
   (->> xs
-       (map #(math/expt % 2) ,,,)
-       (reduce + ,,,)))
+       (map square ,,,)
+       (sum ,,,)))
 
-(defn square-of-sums
+(defn square-of-sum
   "Return the square of sum"
   [xs]
   (->> xs
-       (reduce + ,,,)
-       (#(math/expt % 2) ,,,)))
+       (sum ,,,)
+       (square ,,,)))
 
 (defn problem-6
   "Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."
@@ -136,7 +146,7 @@
    (problem-6 100))
   ([n]
    (let [xs (range (inc n))
-         sq-sum (square-of-sums xs)
+         sq-sum (square-of-sum xs)
          sum-sq  (sum-of-squares xs)]
      (- sq-sum sum-sq))))
 
@@ -150,7 +160,60 @@
         (drop (dec n) ,,,)
         (first ,,,))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn digits
+  "Return a lazy seq of all the digits in x"
+  [x]
+  (->> x
+       (str ,,,)
+       (map (comp read-string str) ,,,)))
+
+
+(defn product
+  "Return the product of coll"
+  [xs]
+  (reduce * xs))
+
+(defn problem-8
+  "Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?"
+  ([]
+   (problem-8 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450))
+  ([x]
+   (->> x
+        (str ,,,)
+        (digits ,,,)
+        (partition 13 1 ,,,)
+        (map #(product %) ,,,)
+        (reduce max ,,,))))
+
+(defn pythagorian-triple-product
+  "Using x, calculate the product of the Pythagorian triple."
+  [x]
+  (let [abc (atom [])]
+    (loop
+      [a 1]
+      (when
+        (<= a (/ x 3))
+        (loop
+          [b (inc a)]
+          (let [c (- x a b)
+                a2 (* a a)
+                b2 (* b b)
+                c2 (* c c)]
+            (when
+              (<= b (/ x 2))
+              (if
+                (= c2 (+ a2 b2))
+                (reset! abc [a b c])
+                (recur (inc b))))))
+      (recur (inc a))))
+    (reduce * @abc)))
+
+(defn problem-9
+  "There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc."
+  ([]
+   (problem-9 1000))
+  ([x]
+   (pythagorian-triple-product x)))
 
 (defn primes-below
   "Generate primes below x using Sieve of Eratosthenes"
@@ -162,7 +225,7 @@
     (loop
       [s sieve
        f 3]
-      (let [ff (math/expt f 2)]
+      (let [ff (square f)]
         (if
           (> ff x)
           (persistent! s)
@@ -178,4 +241,4 @@
   ([x]
    (->> x
         (primes-below ,,,)
-        (apply + ,,,))))
+        (sum ,,,))))
